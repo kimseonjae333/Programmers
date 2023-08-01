461402791921277f0cf8b10ca869a555133a8bb7
SEONJAE KIM <79892278+kimseonjae333@users.noreply.github.com>
2023-08-01 14:11:21 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -2,18 +2,19 @@ import java.util.ArrayList;
 
 class Solution {
     public int solution(int n) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        ArrayList<Integer> yaksu = new ArrayList<>();
         
         for(int i = 1; i <= n; i++){
-            if(n%i == 0) {
-                arr.add(i);
-            }        
+            if(n % i == 0){
+                yaksu.add(i);
+            }
         }
         
         int answer = 0;
-        for(int i = 0; i < arr.size(); i++){
-            answer += arr.get(i);
+        for(int i = 0; i < yaksu.size(); i++){
+            answer += yaksu.get(i);
         }
+        
         return answer;
     }
 }
