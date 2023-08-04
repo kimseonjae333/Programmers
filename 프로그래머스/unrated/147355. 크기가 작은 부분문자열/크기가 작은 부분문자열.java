d61dd1742dbce74b01f1e1d8bf66c05e5da090de
SEONJAE KIM <79892278+kimseonjae333@users.noreply.github.com>
2023-08-04 19:49:21 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        long intP = Long.parseLong(p);
+        long intP = Long.parseLong(p); //p의 길이가 int의 범위를 넘어갈 수 있음
         
         for(int i = 0; i < (t.length()-p.length()+1); i++){
             //System.out.println(Integer.parseInt(t.substring(i,i+p.length())));
